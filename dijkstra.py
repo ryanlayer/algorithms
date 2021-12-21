bc627c8725cdeb7d7a0773d9df5aa0fc37d59c99
Ryan Layer <ryan.layer@gmail.com>
2021-12-21 14:44:50 -0700

    Create dijkstra.py

---
+++ dijkstra.py
@@ -0,0 +1,30 @@
+import heapq
+
+def Dijkstra(G,s,w):
+    dist = [float('inf')] * len(G)
+    dist[s] = 0
+    pred = [None] * len(G)
+    Q = []
+    heapq.heappush(Q,(0,s))
+    
+    while len(Q) > 0:
+        d,x = heapq.heappop(Q)
+        for y in G[x]:
+            if dist[y] > dist[x] + w[(x,y)]:#(x,y) is tense
+                dist[y] = dist[x] + w[(x,y)]
+                pred[y] = x
+                heapq.heappush(Q,(dist[y],y))
+        print(dist,pred,Q)
+
+    return dist, pred
+    
+G = [[1,2],[2,3],[1,4],[1,4],[2,3]]
+
+w = { (0,1) : 1, (1,0) : 1,
+      (0,2) : 3, (2,0) : 3,
+      (1,2) : 1, (2,1) : 1,
+      (1,3) : 3, (3,1) : 3,
+      (2,4) : 7, (4,2) : 7,
+      (3,4) : 2, (4,3) : 2}
+
+dist, pred = Dijkstra(G,0,w)
