8c21640bf6e70fce53dfe2300449d58d7758c7d7
Ryan Layer <ryan.layer@gmail.com>
2022-02-03 11:07:01 -0700

    time and fit

---
+++ asymptotic/run_time_as_function.py
@@ -0,0 +1,59 @@
+import time
+
+import matplotlib
+import pylab
+
+import numpy as np
+
+
+def square(n):
+    r = 0
+    for i in range(n):
+        for j in range(n):
+            r+=1
+    return r
+
+def linear(n):
+    r = 0
+    for i in range(n):
+        r+=1
+    return r
+
+
+fig = matplotlib.pyplot.figure(figsize=(4,4),dpi=300)
+ax = fig.add_subplot(1,1,1)
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+for func in [linear, square]:
+    X = []
+    Y = []
+    for n in range(1,10000,1000):
+        start = time.time()
+        r = func(n)
+        end = time.time()
+        X.append(n)
+        print(n,end-start)
+        Y.append(end - start)
+
+
+    ax.plot(X,Y,'.',lw=0)
+
+    f = np.polyfit(X,Y,2)
+    print(f)
+    p = np.poly1d(f)
+    xp = np.linspace(1,10000,1000)
+
+    ax.plot(xp,p(xp))
+
+    eq = '{:.2e}'.format(float(f[0])) + 'n^2 +'\
+         ' {:.2e}'.format(float(f[1])) + 'n +' \
+         ' {:.2e}'.format(float(f[2]))
+         
+
+    ax.text(X[-1],Y[-1],eq, size=8)
+    
+
+matplotlib.pyplot.savefig('runtime.png', bbox_inches='tight')
+
