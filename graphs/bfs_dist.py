3edbdb626462397aedb16dc6e6fb7118823a5889
Ryan Layer <ryan.layer@gmail.com>
2023-01-27 08:35:10 -0700

    remove extra prints

--- graphs/bfs_dist.py
+++ graphs/bfs_dist.py
@@ -10,16 +10,14 @@ def BFS_dist(V):
     Q.append(0)
     while len(Q) != 0:
         x = Q.pop(0)
-        print('-', x)
         dist[x] = dist[ pred[x] ] + 1
         if state[x] == 'new':
             state[x] = 'old'
             for y in V[x]:
                 if pred[y] == None:
                     pred[y] = x
-                print('+', y)
                 Q.append(y)
-    print(dist)
+    return dist
 
 V = [[1,2,3],[4],[5],[6],[5],[7],[5],[]]
-DFS(V)
+print(BFS_dist(V))
