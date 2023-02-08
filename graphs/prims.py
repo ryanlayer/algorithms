aa87c02b21f58c466cc2aa3d958375a485cff026
Ryan Layer <ryan.layer@gmail.com>
2023-02-08 09:20:53 -0700

    save space

--- graphs/prims.py
+++ graphs/prims.py
@@ -2,33 +2,22 @@ import heapq
 
 def Prims(V, E, w):
     X = {0} # the nodes in the tree, pick any node as the starting node
-
-    
     Q = []
     for y in E[0]:
         heapq.heappush(Q, (w[(0,y)], (0,y)))
-
     T = [] # the edges in the tree
-
     while X != V:
-        #get the min edge u,v where u is in X and v is not in X
-        d,(u,v) = heapq.heappop(Q) # min edge, u is in X, just test X
-
+        # get the min edge u,v where u is in X and v is not in X
+        d,(u,v) = heapq.heappop(Q) # min edge, u in X, test v in X
         while v in X:
-            d,(u,v) = heapq.heappop(Q) # min edge, u is in X, just test X
-
+            d,(u,v) = heapq.heappop(Q) 
         X.add(v)
         T.append([u,v])
-
         for y in E[v]:
             heapq.heappush(Q, (w[(v,y)], (v,y)))
-
     return T
 
-A = 0
-B = 1
-C = 2
-D = 3
+A = 0; B = 1; C = 2; D = 3
 
 V = {A, B, C, D}
 E = [[B, C], [A,C,D], [A,B,D], [B, C]]
