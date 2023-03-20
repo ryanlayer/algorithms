b1c39add437080dabf44bed6f2586d9f679f0275
Ryan Layer <ryan.layer@gmail.com>
2023-03-20 11:13:22 -0600

    plot w and wo fib

--- dynamic_programming/fib.py
+++ dynamic_programming/fib.py
@@ -45,12 +45,12 @@ def fib_bottom_up_1_space(n):
     
 n = int(sys.argv[1])
 
-#fib_times = []
-#for i in range(2,n):
-#    start = time.time()
-#    f_i = fib(i)
-#    end = time.time()
-#    fib_times.append((end-start))
+fib_times = []
+for i in range(2,min(20,n)):
+    start = time.time()
+    f_i = fib(i)
+    end = time.time()
+    fib_times.append((end-start))
 
 fib_memo_times = []
 for i in range(1, n+1):
@@ -84,7 +84,7 @@ ax.spines['right'].set_visible(False)
 ax.set_xlabel('Input size')
 ax.set_ylabel('Time (microsecond)')
 
-#ax.plot(range(1,n), fib_times, '.-', lw=1, label='fib')
+ax.plot(range(2,min(20,n)), fib_times, '.-', lw=1, label='fib')
 ax.plot(range(1,n+1),
         fib_memo_times,
         '.-',
@@ -102,5 +102,31 @@ ax.plot(range(1,n+1),
         label='fib_bottom_up_1_space')
 ax.legend(frameon=False)
 
-matplotlib.pyplot.savefig('runtime.png', bbox_inches='tight')
+matplotlib.pyplot.savefig('runtime_w_fib.png', bbox_inches='tight')
+
+fig = matplotlib.pyplot.figure(figsize=(4,3),dpi=300)
+ax = fig.add_subplot(1,1,1)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.set_xlabel('Input size')
+ax.set_ylabel('Time (microsecond)')
+
+ax.plot(range(1,n+1),
+        fib_memo_times,
+        '.-',
+        lw=1,
+        label='fib_memo')
+ax.plot(range(1,n+1),
+        fib_bottom_up_times,
+        '.-',
+        lw=1,
+       label='fib_bottom_up')
+ax.plot(range(1,n+1),
+        fib_bottom_up_1_space_times,
+        '.-',
+        lw=1,
+        label='fib_bottom_up_1_space')
+ax.legend(frameon=False)
+
+matplotlib.pyplot.savefig('runtime_wo_fib.png', bbox_inches='tight')
 
