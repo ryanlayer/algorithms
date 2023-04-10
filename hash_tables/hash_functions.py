ed24eed43b381c390fac3cdc243d24e59d6dbe6f
Ryan Layer <ryan.layer@gmail.com>
2023-04-10 10:41:57 -0600

    rem the 2 mods

--- hash_tables/hash_functions.py
+++ hash_tables/hash_functions.py
@@ -6,14 +6,11 @@ def h_ascii_sum(key, N):
         s += ord(key[i])
     return s % N
 
-def h_polynomial_rolling(key, N, p=53, m=2**64):
-    '''  p a prime number roughly equal to the number of characters in the input alphabe 
-         m should be a large number, since the probability of two random strings colliding is 
-           about 1/m. Sometimes m=2^64 is chosen'''
+def h_polynomial_rolling(key, N, p=53):
+    # p a prime number roughly equal to the number of characters in the input alphabe 
     s = 0
     for i in range(len(key)):
         s += ord(key[i]) * p**i
-    s = s % m
     return s % N 
 
 def h_python(key, N):
